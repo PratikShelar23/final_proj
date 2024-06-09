4b1ab820f9f19faa22f42abd6f182691c61769fb
PratikShelar23 <119117359+PratikShelar23@users.noreply.github.com>
2024-06-09 21:42:58 +0530

    Update settings.py

--- Just_Mark_Project/settings.py
+++ Just_Mark_Project/settings.py
@@ -78,12 +78,7 @@ DATABASES = {
     'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'attendances',
-        'USER':'admin',
-        'PASSWORD':'Pratik2399',
-        'HOST':'attendb.cfwggeq4oega.ap-south-1.rds.amazonaws.com', 
-        'PORT':'3306',
+     
     }
 }
 
@@ -138,12 +133,3 @@ STATICFILES_DIRS = [
 LOGIN_URL = '/profile/'  
 
 
-AWS_ACCESS_KEY_ID = 'AKIA4C5BUJIRVPRB6YOK'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'pratikatt'
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'ap-south-1'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL =  None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
